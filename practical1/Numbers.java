06239b58701219b5b04ce11f5e6401174ed482ac
Samriddhi-dubey10 <74581936+Samriddhi-dubey10@users.noreply.github.com>
2020-11-23 10:12:18 +0530

    Add files via upload

---
+++ practical1/Numbers.java
@@ -0,0 +1,20 @@
+package practical1;
+import java.util.Scanner;
+public class Numbers {
+	
+	
+
+	
+		public static void main(String []args)
+		{
+			Scanner kb=new Scanner(System.in);
+			System.out.println("Enter two no");
+			int a=kb.nextInt();
+			int b=kb.nextInt();
+			int s=a+b;
+			System.out.println("Sum of "+a+" and "+b+" is "+s);
+		}
+
+	}
+
+
